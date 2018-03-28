c95764081766b9e94b72eefc7b28a663f7ddda83
OleksandrFedosyeyev <fedoseev123@i.ua>
2018-03-28 17:35:19 +0300

    Add project SeaBattle

---
+++ PSJavaSeabattle2-master/src/ru/sergeykravchenko/seabattle/tests/PlayerTest.java
@@ -0,0 +1,33 @@
+package ru.sergeykravchenko.seabattle.tests;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.junit.rules.Timeout;
+import ru.sergeykravchenko.seabattle.gameseabattle.GameSeaBattle;
+import ru.sergeykravchenko.seabattle.player.Player;
+
+/**
+ * *   Java 1 Course work (IntelliJ Idea)
+ * <h5>Java 1 Домашнее задание/курсовая работа. класс тестирования класса Игрока (<code>Player</code>) Игры морской бой/h5>
+ * <p> реализует проверку методов автоматического размещения кораблей
+ * через проверку методов  <code>placePlayerNavy()</code> и <code>place2ndPlayerNavy()</code>
+ * а также TODO: метода настройки игрока</p>
+ * <p>/p>
+ * <p></p>
+ * <p></p>
+ * <p>методы : </p> <ul>
+ * <li</li>
+ * <li></li>
+ * <ul>
+ *
+ * @author Sergey Kravchenko
+ * @version 0.0
+ * @see Created with IntelliJ IDEA. * Date: 22.10.13    * Time: 20:56
+ */
+
+public class PlayerTest {
+
+}
